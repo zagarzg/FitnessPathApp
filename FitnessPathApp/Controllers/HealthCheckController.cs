fc76ff33cca71da7688ba7c995dc29b31b9e595b
zagarzg <zagar.zg@gmail.com>
2022-02-17 22:39:15 +0100

    Add HealthCheckController

---
+++ FitnessPathApp/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.API.Controllers
+{
+
+	public class HealthCheckController : BaseController
+	{
+		[HttpGet]
+		public IActionResult Ping()
+		{
+			return Ok("Healthy");
+		}
+	}
+}
