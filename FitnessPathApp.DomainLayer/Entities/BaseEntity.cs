a643164363ca4aefdc5bb6607c9d7baefc6338af
zagarzg <zagar.zg@gmail.com>
2022-02-18 09:13:17 +0100

    Add BaseEntity model

---
+++ FitnessPathApp.DomainLayer/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using FitnessPathApp.DomainLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.DomainLayer.Entities
+{
+    public class BaseEntity : IEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
