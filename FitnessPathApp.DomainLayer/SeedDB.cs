61e33806f96432100c3a57c92d75c080395e7808
zagarzg <zagar.zg@gmail.com>
2022-02-24 23:24:28 +0100

    Add database seeder

---
+++ FitnessPathApp.DomainLayer/SeedDB.cs
@@ -0,0 +1,56 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.DomainLayer
+{
+    public static class DbSeeder
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.SeedWeightLogs();
+        }
+
+        private static void SeedWeightLogs(this ModelBuilder builder)
+        {
+
+            var logs = new List<WeightLog>
+            {
+                new WeightLog
+                {
+                    Id = Guid.Parse("0faee6ac-1772-4bbe-9990-a7d9a22dd559"),
+                    Value = 77.5,
+                    Date = new DateTime(2022, 2, 14),
+                },
+                new WeightLog
+                {
+                    Id = Guid.Parse("c4714153-23fc-4413-b6dc-7fa230cb8883"),
+                    Value = 78,
+                    Date = new DateTime(2022, 2, 15),
+                },
+                new WeightLog
+                {
+                    Id = Guid.Parse("9d9926f0-ee0b-4cdb-b4bd-c178377d8868"),
+                    Value = 77.3,
+                    Date = new DateTime(2022, 2, 16),
+                },
+                new WeightLog
+                {
+                    Id = Guid.Parse("8514a58d-0edc-46bc-a0c8-bd912b5f5742"),
+                    Value = 77.6,
+                    Date = new DateTime(2022, 2, 17),
+                },
+                new WeightLog
+                {
+                    Id = Guid.Parse("52fce968-8a43-4dbd-ab26-dcf334c149dd"),
+                    Value = 77.8,
+                    Date = new DateTime(2022, 2, 18),
+                }
+            };
+
+            builder.Entity<WeightLog>().HasData(logs);
+        }
+    }
+}
