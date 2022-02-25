44ad1916b22d3efbe49b3580c59d56ae0c364727
zagarzg <zagar.zg@gmail.com>
2022-02-25 17:03:37 +0100

    Add IExerciseService interface

---
+++ FitnessPathApp.BusinessLayer/Interfaces/IExerciseService.cs
@@ -0,0 +1,18 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.BusinessLayer.Interfaces
+{
+    public interface IExerciseService
+    {
+        Task<IEnumerable<Exercise>> GetAll(CancellationToken cancellationToken);
+        Task<Exercise> Get(Guid id, CancellationToken cancellationToken);
+        Task<Exercise> Create(Exercise exercise, CancellationToken cancellationToken);
+        Task<Exercise> Update(Exercise exercise, CancellationToken cancellationToken);
+        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
