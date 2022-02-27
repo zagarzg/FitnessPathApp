84080678df5a6314d826bbd0f8b1aef41fc8c1f7
zagarzg <zagar.zg@gmail.com>
2022-02-27 21:57:46 +0100

    Add ExerciseController

---
+++ FitnessPathApp/Controllers/ExerciseController.cs
@@ -0,0 +1,53 @@
+﻿using FitnessPathApp.BusinessLayer.Interfaces;
+using FitnessPathApp.DomainLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.API.Controllers
+{
+    public class ExerciseController : BaseController
+    {
+		private readonly IExerciseService _exerciseService;
+
+        public ExerciseController(IExerciseService exerciseService)
+        {
+            _exerciseService = exerciseService;
+        }
+
+        [HttpGet]
+		public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+		{
+			return Ok(await _exerciseService.GetAll(cancellationToken));
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+		{
+			return Ok(await _exerciseService.Get(id, cancellationToken));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([FromBody] Exercise exercise, CancellationToken cancellationToken)
+		{
+			return Ok(await _exerciseService.Create(exercise, cancellationToken));
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> Update([FromBody] Exercise exercise, CancellationToken cancellationToken)
+		{
+			await _exerciseService.Update(exercise, cancellationToken);
+			return Ok();
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> Delete([FromBody] Guid id, CancellationToken cancellationToken)
+		{
+			await _exerciseService.Delete(id, cancellationToken);
+			return Ok();
+		}
+	}
+}
