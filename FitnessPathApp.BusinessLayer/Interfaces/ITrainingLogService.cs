e58bed64e9c747ab6245d2ac8712eae9926db295
zagarzg <zagar.zg@gmail.com>
2022-03-01 20:21:54 +0100

    Add ITrainingLog interface

---
+++ FitnessPathApp.BusinessLayer/Interfaces/ITrainingLogService.cs
@@ -0,0 +1,18 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.BusinessLayer.Interfaces
+{
+    public interface ITrainingLogService
+    {
+        Task<IEnumerable<TrainingLog>> GetAll(CancellationToken cancellationToken);
+        Task<TrainingLog> Get(Guid id, CancellationToken cancellationToken);
+        Task<TrainingLog> Create(TrainingLog log, CancellationToken cancellationToken);
+        Task<TrainingLog> Update(TrainingLog log, CancellationToken cancellationToken);
+        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
