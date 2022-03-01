3ee9beb5840aa23060e46cb4f1d1705934ff7103
zagarzg <zagar.zg@gmail.com>
2022-03-01 20:08:50 +0100

    Add TrainingLog model

---
+++ FitnessPathApp.DomainLayer/Entities/TrainingLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.DomainLayer.Entities
+{
+    public class TrainingLog : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public ICollection<Exercise> Exercises { get; set; }
+    }
+}
