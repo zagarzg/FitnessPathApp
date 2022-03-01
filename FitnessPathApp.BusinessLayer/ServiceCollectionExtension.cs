64a1516336db452691d86d343f013e8433913ab3
zagarzg <zagar.zg@gmail.com>
2022-03-01 20:31:06 +0100

    Register TrainingLogService via ServiceCollection Extension pattern

--- FitnessPathApp.BusinessLayer/ServiceCollectionExtension.cs
+++ FitnessPathApp.BusinessLayer/ServiceCollectionExtension.cs
@@ -13,6 +13,7 @@ namespace FitnessPathApp.BusinessLayer
         {
             services.AddTransient<IWeightLogService, WeightLogService>();
             services.AddTransient<IExerciseService, ExerciseService>();
+            services.AddTransient<ITrainingLogService, TrainingLogService>();
         }
     }
 }
