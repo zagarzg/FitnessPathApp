9ac2ebd6ad212b2257d0852c28457d22b1d73c64
zagarzg <zagar.zg@gmail.com>
2022-03-04 19:32:19 +0100

    Add FoodItem model

---
+++ FitnessPathApp.DomainLayer/Entities/FoodItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.DomainLayer.Entities
+{
+    public class FoodItem : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public int Carbs { get; set; }
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+        public Guid FoodLogId { get; set; }
+    }
+}
