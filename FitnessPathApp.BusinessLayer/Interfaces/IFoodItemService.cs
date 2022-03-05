5e02d04a682f360aa620efbd3937aa462b6e3a34
zagarzg <zagar.zg@gmail.com>
2022-03-05 18:38:28 +0100

    Add IFoodItemService interface

---
+++ FitnessPathApp.BusinessLayer/Interfaces/IFoodItemService.cs
@@ -0,0 +1,18 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.BusinessLayer.Interfaces
+{
+    public interface IFoodItemService
+    {
+        Task<IEnumerable<FoodItem>> GetAll(CancellationToken cancellationToken);
+        Task<FoodItem> Get(Guid id, CancellationToken cancellationToken);
+        Task<FoodItem> Create(FoodItem item, CancellationToken cancellationToken);
+        Task<FoodItem> Update(FoodItem item, CancellationToken cancellationToken);
+        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
