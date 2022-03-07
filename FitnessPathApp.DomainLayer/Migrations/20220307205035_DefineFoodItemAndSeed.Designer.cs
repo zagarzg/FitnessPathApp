79ed4093ee92d828ce96ca647ba41fe3e463bfb9
zagarzg <zagar.zg@gmail.com>
2022-03-07 21:56:16 +0100

    Add migrations to define FoodItem model and seed data

--- FitnessPathApp.DomainLayer/Migrations/20220307205035_DefineFoodItemAndSeed.Designer.cs
+++ FitnessPathApp.DomainLayer/Migrations/20220307205035_DefineFoodItemAndSeed.Designer.cs
@@ -4,14 +4,16 @@ using FitnessPathApp.DomainLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FitnessPathApp.DomainLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220307205035_DefineFoodItemAndSeed")]
+    partial class DefineFoodItemAndSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
