f1570cd773e2af49efee1c870d70e47325548232
zagarzg <zagar.zg@gmail.com>
2022-03-10 23:43:51 +0100

    Add IFoodLogService interface

---
+++ FitnessPathApp.BusinessLayer/Interfaces/IFoodLogService.cs
@@ -0,0 +1,18 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.BusinessLayer.Interfaces
+{
+    public interface IFoodLogService
+    {
+        Task<IEnumerable<FoodLog>> GetAll(CancellationToken cancellationToken);
+        Task<FoodLog> Get(Guid id, CancellationToken cancellationToken);
+        Task<FoodLog> Create(FoodLog log, CancellationToken cancellationToken);
+        Task<FoodLog> Update(FoodLog log, CancellationToken cancellationToken);
+        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
