9ff12650fcf281980b58831d1bf694a504cf2159
zagarzg <zagar.zg@gmail.com>
2022-03-14 18:10:04 +0100

    Add IUserService interface

---
+++ FitnessPathApp.BusinessLayer/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.BusinessLayer.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll(CancellationToken cancellationToken);
+        Task<User> Get(Guid id, CancellationToken cancellationToken);
+        Task<User> Create(User user, CancellationToken cancellationToken);
+        Task<User> Update(User user, CancellationToken cancellationToken);
+        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
