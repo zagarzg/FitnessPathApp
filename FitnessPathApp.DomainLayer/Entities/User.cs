2c657a11d65dea365659fdc220fbc6c898aad50b
zagarzg <zagar.zg@gmail.com>
2022-03-14 17:59:52 +0100

    Add User model

---
+++ FitnessPathApp.DomainLayer/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.DomainLayer.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public ICollection<WeightLog> WeightLogs { get; set; }
+        public ICollection<TrainingLog> TrainingLogs { get; set; }
+        public ICollection<FoodLog> FoodLogs { get; set; }
+
+    }
+}
