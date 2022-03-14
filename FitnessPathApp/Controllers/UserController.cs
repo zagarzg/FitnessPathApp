636851187437844b76a28da85a9385e3845ac116
zagarzg <zagar.zg@gmail.com>
2022-03-14 18:44:28 +0100

    Add UserController

---
+++ FitnessPathApp/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using FitnessPathApp.BusinessLayer.Interfaces;
+using FitnessPathApp.DomainLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.API.Controllers
+{
+    public class UserController : BaseController
+    {
+		private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+		public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+		{
+			return Ok(await _userService.GetAll(cancellationToken));
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+		{
+			return Ok(await _userService.Get(id, cancellationToken));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([FromBody] User user, CancellationToken cancellationToken)
+		{
+			return Ok(await _userService.Create(user, cancellationToken));
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> Update([FromBody] User user, CancellationToken cancellationToken)
+		{
+			await _userService.Update(user, cancellationToken);
+			return Ok();
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> Delete([FromBody] Guid id, CancellationToken cancellationToken)
+		{
+			await _userService.Delete(id, cancellationToken);
+			return Ok();
+		}
+	}
+}
