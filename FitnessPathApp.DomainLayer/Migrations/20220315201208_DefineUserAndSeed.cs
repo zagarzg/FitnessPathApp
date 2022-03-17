dee1f7c18ba630bec755c5b57dac270e26d5a89c
zagarzg <zagar.zg@gmail.com>
2022-03-17 23:15:32 +0100

    Add migrations to define User model and seed data

---
+++ FitnessPathApp.DomainLayer/Migrations/20220315201208_DefineUserAndSeed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitnessPathApp.DomainLayer.Migrations
+{
+    public partial class DefineUserAndSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("cd6b8714-4806-4fe6-b28f-90185dbfbdd2"),
+                column: "Password",
+                value: "$2b$10$sZciQJmbX9ABJHVGpuUpaufNsMwo7G5GrKrz3uS33.lvdFpeX2AUa");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("cd6b8714-4806-4fe6-b28f-90185dbfbdd2"),
+                column: "Password",
+                value: "$2b$10$uSC7UwAon9I58xkhmM0.I.4oNNjtgDAtHrInKH7YXsfs.cKAQlfCy");
+        }
+    }
+}
