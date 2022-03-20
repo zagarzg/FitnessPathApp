55d59fea954a8b4473cf4b25a758a716ddf23413
zagarzg <zagar.zg@gmail.com>
2022-03-20 20:51:38 +0100

    Add WeightLog validator

---
+++ FitnessPathApp.BusinessLayer/Validators/WeightLogValidator.cs
@@ -0,0 +1,35 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.BusinessLayer.Validators
+{
+    public class WeightLogValidator : AbstractValidator<WeightLog>
+    {
+        public WeightLogValidator()
+        {
+            RuleFor(log => log.Value)
+                .NotEmpty()
+                .WithMessage("Weight must be set")
+                .GreaterThan(0)
+                .WithMessage("Weight must be greater than 0");
+
+            RuleFor(log => log.Date)
+                .NotEmpty()
+                .WithMessage("Date must be set")
+                .Must(date => IsNotInFuture(date))
+                .WithMessage("Date cannot be set in future");
+
+            RuleFor(log => log.UserId)
+                .NotEmpty()
+                .WithMessage("WeightLog must be connected to specific User");
+        }
+
+        public bool IsNotInFuture(DateTime date)
+        {
+            return date < DateTime.Now;
+        }
+    }
+}
