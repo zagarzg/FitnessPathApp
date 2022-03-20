262cde76c0c3c0b3805a9f82efec6b9ed770432c
zagarzg <zagar.zg@gmail.com>
2022-03-20 13:24:38 +0100

    Add TrainingLog validator

---
+++ FitnessPathApp.BusinessLayer/Validators/TrainingLogValidator.cs
@@ -0,0 +1,29 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPathApp.BusinessLayer.Validators
+{
+    public class TrainingLogValidator : AbstractValidator<TrainingLog>
+    {
+        public TrainingLogValidator()
+        {
+            RuleFor(log => log.Date)
+                .NotEmpty()
+                .WithMessage("Date must be set")
+                .Must(date => IsNotInFuture(date))
+                .WithMessage("Date cannot be set in future");
+
+            RuleFor(log => log.UserId)
+                .NotEmpty()
+                .WithMessage("TrainingLog must be connected to specific User");
+        }
+
+        public bool IsNotInFuture(DateTime date)
+        {
+            return date < DateTime.Now;
+        }
+    }
+}
