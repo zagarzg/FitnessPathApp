55324371eb67c51caa65819d2e163cf3f84083dd
zagarzg <zagar.zg@gmail.com>
2022-09-04 16:43:48 +0200

    Add SideNav component

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideNavComponent } from './navigations/side-nav/side-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideNavComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule
+  ],
+  exports: [SideNavComponent]
 })
 export class SharedModule { }
