3ed6754ba3620e5a3b32b108d437baa42a76bae4
zagarzg <zagar.zg@gmail.com>
2022-09-25 20:45:56 +0200

    Add TrainingLogDTO and its mappings

---
+++ Server/FitnessPathApp.PersistanceLayer/DTOs/TrainingLogDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessPathApp.PersistanceLayer.DTOs
+{
+    public class TrainingLogDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public ICollection<ExerciseDTO> Exercises { get; set; }
+    }
+}
