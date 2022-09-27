04a61888af9a1c97febdf1d8befefe33a3381120
zagarzg <zagar.zg@gmail.com>
2022-09-27 19:00:55 +0200

    Add TrainingLog service on client side

---
+++ Client/src/app/features/training-log/services/training-log.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TrainingLog } from '../models/TrainingLog';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+
+export class TrainingLogService {
+
+  constructor(private _http: HttpClient) { }
+
+  public getTrainingLog(id: string): Observable<TrainingLog> {
+    return this._http.get<TrainingLog>(`${environment.URL}/TrainingLog/Get/${id}`);
+  }
+
+  public getMonthlyTrainingLogs(month: number): Observable<TrainingLog[]> {
+    return this._http.get<TrainingLog[]>(`${environment.URL}/TrainingLog/GetMonthyTrainingLogs?month=${month}`);
+  }
+
+  // public getTrainingLogDays(month:number): Observable<number[]> {
+  //   return this._http.get<number[]>(`${environment.URL}/TrainingLog/GetMonthyTrainingLogDates?month=${month}`);
+  // }
+}
