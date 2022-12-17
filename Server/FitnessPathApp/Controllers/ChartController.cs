97aaf6e110561a27cb9a43e729e7b61f3e71b211
zagarzg <zagar.zg@gmail.com>
2022-12-17 19:56:59 +0100

    Add Chart controller

---
+++ Server/FitnessPathApp/Controllers/ChartController.cs
@@ -0,0 +1,23 @@
+﻿using FitnessPathApp.BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.API.Controllers
+{
+    public class ChartController : BaseController
+    {
+        private readonly IChartService _chartService;
+
+        public ChartController(IChartService chartService)
+        {
+            _chartService = chartService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetChartDataByExerciseName([FromQuery] string exerciseName, [FromQuery] int monthSelected, CancellationToken cancellationToken)
+        {
+            return Ok(await _chartService.GetDataByExerciseName(exerciseName, monthSelected, cancellationToken));
+        }
+    }
+}
