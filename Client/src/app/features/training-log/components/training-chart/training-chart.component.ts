348b7b171dfcb21d00a2cc1e8aa3fd2cf698e40c
zagarzg <zagar.zg@gmail.com>
2022-12-17 19:58:55 +0100

    Refactor Chart component

--- Client/src/app/features/training-log/components/training-chart/training-chart.component.ts
+++ Client/src/app/features/training-log/components/training-chart/training-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterEvent } from '@angular/router';
+import { take } from 'rxjs/operators';
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -11,9 +12,10 @@ import {
   ApexYAxis,
   ApexGrid,
   ApexTitleSubtitle,
-  ApexLegend
-} from "ng-apexcharts";
+  ApexLegend,
+} from 'ng-apexcharts';
 import { TrainingLog } from '../../models/TrainingLog';
+import { ChartService } from '../../services/chart.service';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries | undefined;
@@ -30,114 +32,104 @@ export type ChartOptions = {
 };
 
 interface ChartData {
-  value: number,
-  date: Date
+  value: number;
+  date: Date;
 }
 
 @Component({
   selector: 'app-training-chart',
   templateUrl: './training-chart.component.html',
-  styleUrls: ['./training-chart.component.scss']
+  styleUrls: ['./training-chart.component.scss'],
 })
 export class TrainingChartComponent implements OnInit {
-
-  @ViewChild("chart") chart!: ChartComponent;
+  @ViewChild('chart') chart!: ChartComponent;
   public chartOptions!: Partial<ChartOptions | any>;
 
   @Input() chartData!: TrainingLog[];
 
-  constructor() {}
+  exercises: string[] = ['Bench', 'CGBP'];
+  selectedExercise: string = 'Bench';
+
+  constructor(private _chartService: ChartService) {}
 
   ngOnInit() {
-    this.chartInit();
+    this.exerciseChange('Bench');
   }
 
-  private chartInit() {
-    const benchData = this.chartData.filter(log => log.exercises.length !== 0)
-    .filter(log => log.exercises[0].name == 'Bench')
-    .map(log => {
-      return { y: log.exercises[0].weight, x: new Date(log.date).getDate() }
-    });
+  public exerciseChange(exerciseName: string) {
+    this._chartService
+      .getChartDataByExerciseName(exerciseName, 10)
+      .pipe(take(1))
+      .subscribe((data) => {
+        console.log(data);
+        this.chartInit(data as any);
+      });
+  }
 
-    const cgbpData = this.chartData.filter(log => log.exercises.length !== 0)
-    .filter(log => log.exercises[0].name == 'CGBP')
-    .map(log => {
-      return { y: log.exercises[0].weight, x: new Date(log.date).getDate() }
-    }); 
-  
-    const benchTimes = this.chartData.map(log => new Date(log.date).getDate());
-
+  private chartInit(data: any) {
     this.chartOptions = {
       series: [
         {
-          name: "Bench",
-          data: benchData
+          name: 'Bench',
+          data: data.data,
         },
-        {
-          name: "CGBP",
-          data: cgbpData
-        }
       ],
       chart: {
         animations: {
-          speed           : 400,
+          speed: 400,
           animateGradually: {
-              enabled: false
-          }
+            enabled: false,
+          },
         },
         fontFamily: 'inherit',
-        foreColor : 'inherit',
-        width     : '100%',
-        height    : '300px',
-        type      : 'line',
-        toolbar   : {
-            show: false
+        foreColor: 'inherit',
+        width: '100%',
+        height: '300px',
+        type: 'line',
+        toolbar: {
+          show: false,
         },
-        zoom      : {
-            enabled: false
-        }
+        zoom: {
+          enabled: false,
+        },
       },
-      colors    : ['#818CF8', '#5CFF5C'],
-            dataLabels: {
-                enabled: false
-            },
-            fill      : {
-                colors: ['#312E81']
-            },
+      colors: ['#818CF8', '#5CFF5C'],
+      dataLabels: {
+        enabled: false,
+      },
+      fill: {
+        colors: ['#312E81'],
+      },
       stroke: {
-        curve: "straight"
-      },
-      title: {
-        text: "Training Analytics",
-        align: "left"
+        curve: 'straight',
       },
       markers: {
-        size: 5
+        size: 5,
       },
       xaxis: {
         type: 'category',
         title: {
-          text: "Timeline"
+          text: 'Timeline',
         },
         min: 1,
         max: 26,
-        tickAmount: 25
+        tickAmount: 25,
       },
       yaxis: {
         title: {
-          text: "Weight"
+          text: 'Weight',
         },
-        min: 40,
-        max: 62.5,
-        tickAmount: 9
+        min: data.yMin,
+        max: data.yMax,
+        tickAmount: data.tickAmount,
       },
       legend: {
-        position: "top",
-        horizontalAlign: "right",
+        position: 'top',
+        horizontalAlign: 'right',
         floating: true,
         offsetY: -25,
-        offsetX: -5
-      }
+        offsetX: -5,
+      },
     };
   }
 }
