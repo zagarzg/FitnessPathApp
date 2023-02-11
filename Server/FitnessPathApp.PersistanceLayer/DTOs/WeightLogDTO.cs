4952457f883c8742a08888641d64ac2de556931d
zagarzg <zagar.zg@gmail.com>
2023-02-11 14:34:57 +0100

    Add WeightLogDTO and mappings

---
+++ Server/FitnessPathApp.PersistanceLayer/DTOs/WeightLogDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.PersistanceLayer.DTOs
+{
+    public class WeightLogDTO
+    {
+        public Guid Id { get; set; }
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+        
+    }
+}
