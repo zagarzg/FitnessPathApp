a7acfc5a599c83a20fbb0665de34e37a524af03d
zagarzg <zagar.zg@gmail.com>
2023-02-13 19:08:59 +0100

    Refactor WeightLog page

---
+++ Client/src/app/features/weight-log/components/weight-log-list/weight-log-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { WeightLog } from '../../models/WeightLog';
+
+@Component({
+  selector: 'app-weight-log-list',
+  templateUrl: './weight-log-list.component.html',
+  styleUrls: ['./weight-log-list.component.scss'],
+})
+export class WeightLogListComponent implements OnInit {
+  selected!: Date | null;
+  selectedWeightLog: WeightLog | undefined;
+
+  @Input() weightLogs!: WeightLog[];
+  @Output() selectedDayChangeEvent = new EventEmitter<string | null>();
+  @Output() deleteWeightLogEvent = new EventEmitter<string>();
+  @Output() addWeightLogEvent = new EventEmitter<WeightLog>();
+  @Output() updateWeightLogEvent = new EventEmitter<WeightLog>();
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    console.log(this.weightLogs);
+  }
+
+  selectDay() {
+    this.selectedWeightLog = this.weightLogs.find(
+      (log) =>
+        new Date(log.date).getDate() === this.selected?.getDate() &&
+        new Date(log.date).getMonth() === this.selected?.getMonth() &&
+        new Date(log.date).getFullYear() === this.selected.getFullYear()
+    );
+
+    if (this.selectedWeightLog) {
+      this.selectedDayChangeEvent.emit(this.selectedWeightLog.id);
+    } else {
+      this.selectedDayChangeEvent.emit(null);
+    }
+  }
+
+  addWeightLog() {
+    this.addWeightLogEvent.emit();
+  }
+
+  deleteWeightLog(id: string) {
+    this.deleteWeightLogEvent.emit(id);
+  }
+
+  dateClass() {
+    return (date: Date): MatCalendarCellCssClasses => {
+      if (this.weightLogs == undefined) return '';
+
+      let weightLogsByMonth = this.weightLogs.filter(
+        (log: WeightLog) =>
+          new Date(log.date).getFullYear() == date.getFullYear() &&
+          new Date(log.date).getMonth() == date.getMonth()
+      );
+
+      let mappedLogs = weightLogsByMonth.map((log: WeightLog) => {
+        return new Date(log.date).getDate();
+      });
+
+      const dayNumber: number = date.getDate();
+
+      if (mappedLogs.includes(dayNumber)) {
+        return 'bg-green-300 rounded-full';
+      } else {
+        return '';
+      }
+    };
+  }
+}
--- Client/src/app/features/weight-log/pages/weight-log-page/weight-log-page.component.ts
+++ Client/src/app/features/weight-log/pages/weight-log-page/weight-log-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { WeightLog } from '../../models/WeightLog';
+import { WeightLogService } from '../../services/weight-log.service';
 
 @Component({
   selector: 'app-weight-log-page',
   templateUrl: './weight-log-page.component.html',
-  styleUrls: ['./weight-log-page.component.scss']
+  styleUrls: ['./weight-log-page.component.scss'],
 })
 export class WeightLogPageComponent implements OnInit {
+  public weightLogs$: Observable<WeightLog[]> =
+    this._weightLogService.getAllWeightLogs();
 
-  constructor() { }
+  constructor(private _weightLogService: WeightLogService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAdd(log: WeightLog) {
+    console.log(`Adding log: ${{ log }}`);
   }
 
+  onGet(id: string | null) {
+    console.log(`Getting log by id ${{ id }}`);
+  }
+
+  onDelete(id: string): void {
+    console.log('Deleting weight log!!');
+    this._weightLogService.deleteWeightLog(id).pipe(take(1)).subscribe();
+  }
 }
