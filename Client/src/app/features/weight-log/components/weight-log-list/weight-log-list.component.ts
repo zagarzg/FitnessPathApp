8c23531a100a9cd78f0656ffcfdf127ce38a077c
zagarzg <zagar.zg@gmail.com>
2023-02-19 21:51:53 +0100

    Handle calendar not refreshing issues

--- Client/src/app/features/weight-log/components/weight-log-list/weight-log-list.component.ts
+++ Client/src/app/features/weight-log/components/weight-log-list/weight-log-list.component.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import {
+  MatCalendar,
+  MatCalendarCellCssClasses,
+} from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
 import { WeightLog } from '../../models/WeightLog';
 import { WeightLogFormComponent } from '../weight-log-form/weight-log-form.component';
@@ -9,32 +21,38 @@ import { WeightLogFormComponent } from '../weight-log-form/weight-log-form.compo
   templateUrl: './weight-log-list.component.html',
   styleUrls: ['./weight-log-list.component.scss'],
 })
-export class WeightLogListComponent implements OnInit {
-  selected!: Date | null;
-  selectedWeightLog: WeightLog | undefined;
-
+export class WeightLogListComponent implements OnInit, OnChanges {
+  @Input() selectedDate!: Date | null;
+  @Input() selectedWeightLog: WeightLog | undefined;
   @Input() weightLogs!: WeightLog[];
-  @Output() selectedDayChangeEvent = new EventEmitter<string | null>();
+
+  @Output() selectedDayChangeEvent = new EventEmitter<WeightLog | null>();
   @Output() deleteWeightLogEvent = new EventEmitter<string>();
   @Output() addWeightLogEvent = new EventEmitter<WeightLog>();
   @Output() updateWeightLogEvent = new EventEmitter<WeightLog>();
+  @ViewChild(MatCalendar) calendar!: MatCalendar<Date>;
 
   constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
-    console.log(this.weightLogs);
+  ngOnChanges(changes: SimpleChanges) {
+    let logsChange = changes['weightLogs'];
+    if (logsChange != undefined && !logsChange.firstChange) {
+      this.calendar.updateTodaysDate();
+    }
   }
+
+  ngOnInit(): void {}
 
   selectDay() {
     this.selectedWeightLog = this.weightLogs.find(
       (log) =>
-        new Date(log.date).getDate() === this.selected?.getDate() &&
-        new Date(log.date).getMonth() === this.selected?.getMonth() &&
-        new Date(log.date).getFullYear() === this.selected.getFullYear()
+        new Date(log.date).getDate() === this.selectedDate?.getDate() &&
+        new Date(log.date).getMonth() === this.selectedDate?.getMonth() &&
+        new Date(log.date).getFullYear() === this.selectedDate.getFullYear()
     );
 
     if (this.selectedWeightLog) {
-      this.selectedDayChangeEvent.emit(this.selectedWeightLog.id);
+      this.selectedDayChangeEvent.emit(this.selectedWeightLog);
     } else {
       this.selectedDayChangeEvent.emit(null);
     }
@@ -44,7 +62,8 @@ export class WeightLogListComponent implements OnInit {
     const dialogRef = this.dialog.open(WeightLogFormComponent, {
       width: '400px',
       data: {
-        trainingLogId: this.selectedWeightLog?.id,
+        date: this.selectedDate,
+        weightLogId: this.selectedWeightLog?.id,
       },
     });
 
