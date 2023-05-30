fb3ced06b04801f82335ad18dd0421217355cc62
zagarzg <zagar.zg@gmail.com>
2023-05-30 11:46:44 +0200

    Add FoodItem and FoodLog DTOs and their mappings

---
+++ Server/FitnessPathApp.PersistanceLayer/DTOs/FoodItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.PersistanceLayer.DTOs
+{
+    public class FoodItemDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public int Carbs { get; set; }
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+    }
+}
---
+++ Server/FitnessPathApp.PersistanceLayer/DTOs/FoodLogDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPathApp.PersistanceLayer.DTOs
+{
+    public class FoodLogDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public ICollection<FoodItemDTO> FoodItems { get; set; }
+    }
+}
