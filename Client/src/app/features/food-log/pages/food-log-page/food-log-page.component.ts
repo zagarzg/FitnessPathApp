cfeacff4cbb35ff19e6d191761d2d81ad56a5b58
zagarzg <zagar.zg@gmail.com>
2023-06-04 21:28:10 +0200

    Implement FoodList component inside FoodLog page

--- Client/src/app/features/food-log/pages/food-log-page/food-log-page.component.ts
+++ Client/src/app/features/food-log/pages/food-log-page/food-log-page.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { FoodItem } from '../../models/FoodItem';
+import { FoodLog } from '../../models/FoodLog';
+import { FoodLogService } from '../../services/food-log.service';
 
 @Component({
   selector: 'app-food-log-page',
   templateUrl: './food-log-page.component.html',
-  styleUrls: ['./food-log-page.component.scss']
+  styleUrls: ['./food-log-page.component.scss'],
 })
 export class FoodLogPageComponent implements OnInit {
+  private foodLogsSubject$: BehaviorSubject<FoodLog[]> = new BehaviorSubject<
+    FoodLog[]
+  >([]);
+  public foodLogs$: Observable<FoodLog[]> =
+    this.foodLogsSubject$.asObservable();
 
-  constructor() { }
+  public foodItems!: FoodItem[];
+  public selectedDate!: Date | null;
+
+  constructor(private _foodLogService: FoodLogService) {}
 
   ngOnInit(): void {
+    console.log('NgOnInit of page');
+    this._foodLogService
+      .getAllFoodLogs()
+      .pipe(take(1))
+      .subscribe((logs) => {
+        this.foodLogsSubject$.next(logs);
+      });
   }
 
+  fetchFoodItems(
+    selectDayObject: { foodLogId: string | null; date: Date | null } | null
+  ): void {
+    this.selectedDate = selectDayObject!.date;
+
+    if (!selectDayObject!.foodLogId) {
+      this.foodItems = [];
+      return;
+    }
+    this._foodLogService
+      .getFoodLog(selectDayObject!.foodLogId)
+      .pipe(take(1))
+      .subscribe((res) => (this.foodItems = res.foodItems));
+  }
 }
