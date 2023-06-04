c00d2d9b949a611f4cb5b9fefd4f47dfc392c651
zagarzg <zagar.zg@gmail.com>
2023-06-04 22:50:11 +0200

    Implement CRUD actions for FoodLog on client side

--- Client/src/app/features/food-log/pages/food-log-page/food-log-page.component.ts
+++ Client/src/app/features/food-log/pages/food-log-page/food-log-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, switchMap } from 'rxjs/operators';
 import { FoodItem } from '../../models/FoodItem';
 import { FoodLog } from '../../models/FoodLog';
+import { FoodItemService } from '../../services/food-item.service';
 import { FoodLogService } from '../../services/food-log.service';
 
 @Component({
@@ -20,7 +21,10 @@ export class FoodLogPageComponent implements OnInit {
   public foodItems!: FoodItem[];
   public selectedDate!: Date | null;
 
-  constructor(private _foodLogService: FoodLogService) {}
+  constructor(
+    private _foodLogService: FoodLogService,
+    private _foodItemService: FoodItemService
+  ) {}
 
   ngOnInit(): void {
     console.log('NgOnInit of page');
@@ -45,5 +49,60 @@ export class FoodLogPageComponent implements OnInit {
       .getFoodLog(selectDayObject!.foodLogId)
       .pipe(take(1))
       .subscribe((res) => (this.foodItems = res.foodItems));
+  }
+
+  onAdd(foodItem: FoodItem) {
+    if (foodItem.foodLogId === '') {
+      const foodLog = {
+        id: '00000000-0000-0000-0000-000000000000',
+        date: this.selectedDate!,
+        foodItems: [],
+        userId: '',
+      };
+      this._foodLogService
+        .createFoodLog(foodLog)
+        .pipe(
+          switchMap((foodLog): any => {
+            foodItem.foodLogId = foodLog.id;
+            const updatedLogs = [...this.foodLogsSubject$.value, foodLog];
+            this.foodLogsSubject$.next(updatedLogs);
+            return this._foodItemService.createFoodItem(foodItem).pipe(take(1));
+          })
+        )
+        .subscribe((result: any): any => {
+          this.foodItems = [...this.foodItems, result];
+          // this.chartComponent.monthChange(2);
+        });
+    } else {
+      this._foodItemService
+        .createFoodItem(foodItem)
+        .pipe(take(1))
+        .subscribe((result) => {
+          this.foodItems = [...this.foodItems, result];
+        });
+    }
+  }
+
+  onUpdate(foodItem: FoodItem) {
+    this._foodItemService
+      .updateFoodItem(foodItem)
+      .pipe(take(1))
+      .subscribe((result) => {
+        const updatedItems = this.foodItems.map((el) =>
+          el.id === foodItem.id ? foodItem : el
+        );
+        this.foodItems = updatedItems;
+      });
+  }
+
+  onDelete(id: string): void {
+    this._foodItemService
+      .deleteFoodItem(id)
+      .pipe(take(1))
+      .subscribe(() => {
+        this.foodItems = this.foodItems.filter(
+          (foodItem) => foodItem.id !== id
+        );
+      });
   }
 }
