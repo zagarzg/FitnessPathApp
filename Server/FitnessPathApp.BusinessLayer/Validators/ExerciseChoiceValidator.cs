d568725f62eb595a32884432558458ad3aaf2314
zagarzg <zagar.zg@gmail.com>
2023-06-23 10:58:48 +0200

    Create validator for ExerciseChoice model

---
+++ Server/FitnessPathApp.BusinessLayer/Validators/ExerciseChoiceValidator.cs
@@ -0,0 +1,20 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using FluentValidation;
+
+namespace FitnessPathApp.BusinessLayer.Validators
+{
+    public class ExerciseChoiceValidator : AbstractValidator<ExerciseChoice>
+    {
+        public ExerciseChoiceValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .WithMessage("Exercise name must be set");
+
+            RuleFor(e => e.ExerciseType)
+                .NotEmpty()
+                .WithMessage("Exercise type must be set");
+
+        }
+    }
+}
