caba234e59ebb7510d029ddcc5ce65581239caaa
zagarzg <zagar.zg@gmail.com>
2023-06-23 12:29:26 +0200

    Add ExerciseChoiceDTO and mappings

---
+++ Server/FitnessPathApp.PersistanceLayer/DTOs/ExerciseChoiceDTO.cs
@@ -0,0 +1,14 @@
+﻿using FitnessPathApp.DomainLayer.Entities;
+using System;
+
+namespace FitnessPathApp.PersistanceLayer.DTOs
+{
+    public class ExerciseChoiceDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ExerciseType ExerciseType { get; set; }
+        public byte[] ImageData { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
