5af15d266b4aa7c49244f66a659827febffdc7b6
zagarzg <zagar.zg@gmail.com>
2023-06-24 22:02:34 +0200

    Implement Base64Converter and add mappings

--- Server/FitnessPathApp.BusinessLayer/Mappings.cs
+++ Server/FitnessPathApp.BusinessLayer/Mappings.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FitnessPathApp.DomainLayer.Entities;
 using FitnessPathApp.PersistanceLayer.DTOs;
+using System.Collections.Generic;
 
 namespace FitnessPathApp.BusinessLayer
 {
@@ -14,6 +15,17 @@ namespace FitnessPathApp.BusinessLayer
             CreateMap<WeightLog, WeightLogDTO>().ReverseMap();
             CreateMap<FoodItem, FoodItemDTO>().ReverseMap();
             CreateMap<FoodLog, FoodLogDTO>().ReverseMap();
+            CreateMap<byte[], string>().ConvertUsing<Base64Converter>();
+            CreateMap<string, byte[]>().ConvertUsing<Base64Converter>();
+        }
+
+        private class Base64Converter : ITypeConverter<string, byte[]>, ITypeConverter<byte[], string>
+        {
+            public byte[] Convert(string source, byte[] destination, ResolutionContext context)
+                => System.Convert.FromBase64String(source);
+
+            public string Convert(byte[] source, string destination, ResolutionContext context)
+                => System.Convert.ToBase64String(source);
         }
     }
 }
