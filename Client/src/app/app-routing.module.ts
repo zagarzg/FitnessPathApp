0a0dcba9a66b6072ef777e8eee6f5058d29cafa9
zagarzg <zagar.zg@gmail.com>
2023-08-19 13:25:33 +0200

    Add Settings page and register it

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './core/layout/layout.component';
 import { FoodLogPageComponent } from './features/food-log/pages/food-log-page/food-log-page.component';
+import { SettingsPageComponent } from './features/settings/pages/settings-page/settings-page.component';
 import { TrainingLogPageComponent } from './features/training-log/pages/training-log-page/training-log-page.component';
 import { WeightLogPageComponent } from './features/weight-log/pages/weight-log-page/weight-log-page.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
       {
         path: 'food-log',
         component: FoodLogPageComponent,
+      },
+      {
+        path: 'settings',
+        component: SettingsPageComponent,
       },
     ],
   },
